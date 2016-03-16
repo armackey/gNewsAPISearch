e11fdac1563efaa4934a767082bb9edae0e55bf6
RedWood <mackey91@gmail.com>
2016-03-16 11:24:36 -0700

    adding layer over image

--- client/js/main.js
+++ client/js/main.js
@@ -1,11 +1,25 @@
 $(document).ready(function() {
 
 var deferred = $.Deferred();
+var previousSearches = [];
 
-$('#currentTopic').text('Awaiting for you\'re search..');
+// pushes the user to search
+$('#currentTopic').text('Awaiting for your search..');
+
+// toggles opacity of our input box
+$('input').click(function() {
+  $(this).css('opacity', '1');
+}).
+  blur(function() {
+   $(this).css('opacity', '0'); 
+  });
+
 
 // *** create function to remove previous search ***
-
+function deletePreviousContent(arg) {
+  deferred.resolve($(arg).remove());
+  return deferred.promise();
+}
 
 function placeUserInput(arg) {
   return $('#currentTopic').text(arg);
@@ -13,8 +27,7 @@ function placeUserInput(arg) {
 
 function Elem(val) {
   this.val = val;
-console.log(this.val);
-  $('<div><img src='+ this.val.image.url +'></div>').appendTo($('.images'));
+  $('<div class="add-content"><img src='+ this.val.image.url +'></div>').appendTo($('.images'));
 }
 
 function sendData() {
@@ -23,60 +36,54 @@ function sendData() {
   };
 
   $.post("/topic", user, function(data) {
-    // iterateOverData(data);
     if (!data || null || undefined) {
       $('#currentTopic').text('Data not found. Please try your search again');
     }
 
-    data.forEach(function(elem, i) {
-      
+    setPreviousSearchesOnDom(previousSearches);
+
+    data.forEach(function(elem, i) {        
       if (elem.image) {
         var content = new Elem(elem);  
       }
-      
     });
-    
-    
 
   }).then(function() {
+    $('input').css('opacity', '0');
+    previousSearches.push(user.input);
     placeUserInput(user.input);
   });
 }
-
-  $('#requestButton').click(function() {
-    sendData();
-  });
+  // for pressing the submit button
+  // $('#requestButton').click(function() {
+  //   sendData();
+  // });
 
   // some users may like to press enter inside an input box instead of selecting submit
   $('#userInput').keydown(function(e) {
     if (e.which == 13) {
       e.preventDefault();
-      sendData();
+      if ($('.images > .add-content').length) {
+        $('.images > div').fadeOut(900, function() {
+          deletePreviousContent(this);
+        });
+      }
+      sendData();  
     }
   });
 
-  // function iterateOverData(arg) {
-  //   for (var i = 0; i < arg.length; i+=1) {
-  //     if (!arg[i]) {
-  //       console.log('no data found');
-  //     }
-
-  //     // for (var key in arg[i]) {
-  //     //   placeDataOnToDom(arg[i][key]);  
-  //     // }
-      
-  //   }
-  // }
-
-  // function placeDataOnToDom(arg) {
-  //   console.log(arg);
-  //   if (arg.image) {
-  //     for (var key in arg.image) {
-  //       // console.log(arg.image);
-  //       $('#images > img:first-child').attr('src', arg.image.url);  
-  //     }
-  //   }
-  // }
+  // purpose for this is show users previous searches
+  function setPreviousSearchesOnDom(arg) {
+    arg.forEach(function(elem, i) {
+      if (i === 1) {
+        return;
+      }
+      if ($('#previousTopic').text()) {
+        $('#previousTopic').css({"left": "-=10", "opacity": "1"});
+      }
+      $('#previousTopic').text(elem).animate({"left": "10px", "opacity": ".4"}, 900);
+    });
+  }
 
 
 
